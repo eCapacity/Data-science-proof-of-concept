8f433cfb8a1710b35f6477aa69278d8778a4b629
Jacqueline Feuerborn <jf@ecapacity.dk>
2018-09-13 16:06:15 +0200

    progress

--- script.R
+++ script.R
@@ -10,6 +10,7 @@ install.packages('arules')
 library(googleAnalyticsR)
 library(arules)
 library(tidyr)
+library(dplyr)
 
 
 
@@ -38,4 +39,41 @@ ga_data <- google_analytics(viewId="37002615",
 
 new_data <- ga_data[,c("itemQuantity","productName","transactionId")]
 
-test <- spread(new_data,"productName","itemQuantity")
+
+#looks just at transactionId and product, not quantities and lists products per ttransaction
+temp <- new_data %>% group_by(transactionId) %>% summarise(products =list(productName))
+
+#seperates list of products and makes into comma seperated string
+temp$products <- lapply(temp$products,function(x){paste(unlist(x),collapse = ',')})
+
+#splits comma seperated strings into column per product
+#No one in this data has more than 4 different products
+temp2 <- temp %>% separate("products",c('1','2','3','4'),sep=',')
+
+#remove excess columns
+temp2 <- data.frame(temp2[,c('1','2','3','4')])
+
+
+
+transaction_data <- lapply(temp2,function(x){as.factor(x)})
+
+
+transaction_data <- as(transaction_data,"transactions")
+
+#generate number of unique values in each column (eg. 11 unique quantities, 3167 unique product names and 6325 unique transaction ids)
+size(head(transaction_data))
+
+
+LIST(head(transaction_data,2))
+
+
+
+
+
+
+
+
+
+
+
+
