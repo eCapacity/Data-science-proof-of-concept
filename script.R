4a1a04b61209a73fb11fc1a4ce95b3b3f4f87317
Jacqueline Feuerborn <jf@ecapacity.dk>
2018-09-14 15:03:27 +0200

    data formating completed

--- script.R
+++ script.R
@@ -35,45 +35,50 @@ ga_data <- google_analytics(viewId="37002615",
                               date_range=c(som(som(Sys.Date())-1),som(Sys.Date())-1),
                               metrics=c("ga:itemQuantity","ga:itemRevenue"),
                               dimensions=c("ga:productName","transactionId"),anti_sample=TRUE)
+# 
+# 
+# new_data <- ga_data[,c("itemQuantity","productName","transactionId")]
+# 
+# 
+# #looks just at transactionId and product, not quantities and lists products per ttransaction
+# temp <- new_data %>% group_by(transactionId) %>% summarise(products =list(productName))
+# 
+# #seperates list of products and makes into comma seperated string
+# temp$products <- lapply(temp$products,function(x){paste(unlist(x),collapse = ',')})
+# 
+# #splits comma seperated strings into column per product
+# #No one in this data has more than 4 different products
+# temp2 <- temp %>% separate("products",c('1','2','3','4'),sep=',')
+# 
+# #remove excess columns
+# temp2 <- data.frame(temp2[,c('1','2','3','4')])
+# 
+# 
+# 
+# transaction_data <- sapply(temp2,function(x){as.factor(x)})
+# 
+# 
+# transaction_data <- as(transaction_data,"transactions")
+# 
+# #generate number of unique values in each column (eg. 11 unique quantities, 3167 unique product names and 6325 unique transaction ids)
+# size(head(transaction_data))
+# 
+# 
+# LIST(head(transaction_data,2))
 
+#taking just the transactionId and productName columns from the dataset
+new_data <- ga_data[,c("transactionId","productName")]
 
-new_data <- ga_data[,c("itemQuantity","productName","transactionId")]
+#changing the columns to the class factor
+new_data <- lapply(new_data,function(x){as.factor(x)})
 
+#changing the dataframe to transaction data
+transactions <- as(new_data,"transactions")
 
-#looks just at transactionId and product, not quantities and lists products per ttransaction
-temp <- new_data %>% group_by(transactionId) %>% summarise(products =list(productName))
+rules <- apriori(transactions, parameter = list(support = 0.5, confidence = 0.5))
 
-#seperates list of products and makes into comma seperated string
-temp$products <- lapply(temp$products,function(x){paste(unlist(x),collapse = ',')})
-
-#splits comma seperated strings into column per product
-#No one in this data has more than 4 different products
-temp2 <- temp %>% separate("products",c('1','2','3','4'),sep=',')
-
-#remove excess columns
-temp2 <- data.frame(temp2[,c('1','2','3','4')])
-
-
-
-transaction_data <- sapply(temp2,function(x){as.factor(x)})
-
-
-transaction_data <- as(transaction_data,"transactions")
-
-#generate number of unique values in each column (eg. 11 unique quantities, 3167 unique product names and 6325 unique transaction ids)
-size(head(transaction_data))
-
-
-LIST(head(transaction_data,2))
-
-
-
-
-
-
-
-
-
+ 
+ 
 
 
 
