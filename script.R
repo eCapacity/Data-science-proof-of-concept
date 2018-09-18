bf25f47b4963d9e0a41a86dec7773262e709b8c0
Jacqueline Feuerborn <jf@ecapacity.dk>
2018-09-18 11:01:26 +0200

    Initial skew modification

--- script.R
+++ script.R
@@ -36,10 +36,20 @@ ga_data <- google_analytics(viewId="37002615",
                               date_range=c(som(som(Sys.Date())-40),som(Sys.Date())-1),
                               metrics=c("ga:itemQuantity","ga:itemRevenue"),
                               dimensions=c("ga:productName","transactionId"),anti_sample=TRUE)
-# 
-# 
-# new_data <- ga_data[,c("itemQuantity","productName","transactionId")]
-# 
+
+
+#subset data
+new_data <- ga_data[,c("productName","transactionId")]
+
+#modify skew to remove window size because that is specific to a user
+for(i in 1: length(new_data$productName)){
+  temp <- strsplit(as.character(ga_data$productName[i]),split=" ")
+  if(length(temp[[1]])>2){
+    new_data$productName[i] <- paste(temp[[1]][1],temp[[1]][3])
+  }
+}
+
+
 # 
 # #looks just at transactionId and product, not quantities and lists products per ttransaction
 # temp <- new_data %>% group_by(transactionId) %>% summarise(products =list(productName))
@@ -68,7 +78,7 @@ ga_data <- google_analytics(viewId="37002615",
 # LIST(head(transaction_data,2))
 
 #changing the data to transaction data
-trans <- as(split(ga_data[,"productName"],ga_data[,"transactionId"]),"transactions")
+trans <- as(split(new_data[,"productName"],new_data[,"transactionId"]),"transactions")
 
 #see which items are purchased the most frequently (just number of times it appears in transaction (not quantity))
 frequentItems <- eclat(trans,parameter = list(supp=0.001,maxlen=15))
@@ -89,7 +99,7 @@ rules_lift <- sort (rules, by="lift", decreasing=TRUE)
 inspect(head(rules_lift)) 
 
 #find rules when ZZZ 220 is purchased
-rules <- apriori (data=trans, parameter=list (supp=0.001,conf = 0.05,minlen=2), appearance = list (default="rhs",lhs="ZZZ 220"), control = list (verbose=F))
+rules <- apriori (data=trans, parameter=list (supp=0.001,conf = 0.05,minlen=2), appearance = list (default="rhs",lhs="MHL 5060D"), control = list (verbose=F))
 
 #sort and view top rules by confidence
 rules_conf <- sort (rules, by="confidence", decreasing=TRUE) # 'high-confidence' rules.
